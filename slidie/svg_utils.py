0d12e8eee43849549bdb5df065dba893075d1260
Jonathan Heathcote <mail@jhnet.co.uk>
2023-12-27 21:55:19 +0000

    Add page colouring routines

--- slidie/svg_utils.py
+++ slidie/svg_utils.py
@@ -10,11 +10,13 @@ from xml.etree import ElementTree as ET
 # Relevant XML namespace URIs used by SVGs
 SVG_NAMESPACE = "http://www.w3.org/2000/svg"
 INKSCAPE_NAMESPACE = "http://www.inkscape.org/namespaces/inkscape"
+SODIPODI_NAMESPACE = "http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
 XLINK_NAMESPACE = "http://www.w3.org/1999/xlink"
 
 namespaces = {
     "svg": SVG_NAMESPACE,
     "inkscape": INKSCAPE_NAMESPACE,
+    "sodipodi": SODIPODI_NAMESPACE,
     "xlink": XLINK_NAMESPACE,
 }
 
@@ -61,3 +63,79 @@ def get_inkscape_layer_name(layer: ET.Element) -> str:
     name = layer.attrib.get(f"{{{INKSCAPE_NAMESPACE}}}label")
     assert name is not None
     return name
+
+
+def get_inkscape_page_colour(svg: ET.Element) -> str | None:
+    """
+    Get the Inkscape page colour specified in an SVG, or None for a
+    non-Inkscape SVG.
+    """
+    named_view = svg.find(f".//{{{SODIPODI_NAMESPACE}}}namedview")
+    if named_view is None:  # Probably not an Inkscape SVG
+        return None
+    return named_view.get("pagecolor", None)
+
+
+class ViewBox(NamedTuple):
+    x: float
+    y: float
+    width: float
+    height: float
+
+
+def get_view_box(svg: ET.Element) -> ViewBox | None:
+    """Get the view box for an SVG (if defined)."""
+    if view_box_str := svg.get("viewBox", None):
+        return ViewBox(*map(float, view_box_str.split()))
+    else:
+        return None
+
+
+def get_inkscape_pages(svg: ET.Element) -> list[ViewBox]:
+    """
+    Get the extents of pages in an Inkscape (1.2+) multi-page SVG.
+    
+    For older Inkscape SVGs (without multiple pages) or non-Inkscape SVGs,
+    returns an empty list.
+    """
+    # Also consider pages as candidates (in Inkscape 1.2+)
+    if view_box := get_view_box(svg):
+        x = view_box.x
+        y = view_box.y
+    else:
+        x = y = 0
+    
+    return [
+        ViewBox(
+            float(page_elem.get("x", 0)) + x,
+            float(page_elem.get("y", 0)) + y,
+            float(page_elem.get("width", 0)),
+            float(page_elem.get("height", 0)),
+        )
+        for page_elem in svg.findall(
+            f".//{{{SODIPODI_NAMESPACE}}}namedview/{{{INKSCAPE_NAMESPACE}}}page"
+        )
+    ]
+
+
+def fill_inkscape_page_background(svg: ET.Element) -> None:
+    """
+    Given an Inkscape SVG, add a <rect> behind every page which explicitly
+    draws the Inkscape 'page' colour (100% opaquely).
+    
+    Does nothing for non-Inkscape SVGs.
+    """
+    if page_colour := get_inkscape_page_colour(svg):
+        pages = get_inkscape_pages(svg)
+        if view_box := get_view_box(svg):
+            pages.append(view_box)
+        
+        for page in set(pages):
+            rect = ET.Element(f"{{{SVG_NAMESPACE}}}rect")
+            rect.set("x", str(page.x))
+            rect.set("y", str(page.y))
+            rect.set("width", str(page.width))
+            rect.set("height", str(page.height))
+            rect.set("style", f"fill:{page_colour}")
+            
+            svg.insert(0, rect)
--- tests/test_svg_utils.py
+++ tests/test_svg_utils.py
@@ -1,8 +1,15 @@
+import pytest
+
 from svgs import get_svg
 
 from slidie.svg_utils import (
     enumerate_inkscape_layers,
     get_inkscape_layer_name,
+    get_inkscape_page_colour,
+    ViewBox,
+    get_view_box,
+    get_inkscape_pages,
+    fill_inkscape_page_background,
 )
 
 
@@ -32,3 +39,36 @@ def test_enumerate_inkscape_layers() -> None:
 
     assert len(layers[1].children[0].children) == 0
     assert len(layers[1].children[1].children) == 0
+
+
+@pytest.mark.parametrize(
+    "test_file",
+    [
+        "red_page_colour.svg",
+        "transparent_red_page_colour.svg",
+    ],
+)
+def test_get_inkscape_page_colour(test_file: str) -> None:
+    assert get_inkscape_page_colour(get_svg(test_file)) == "#ff0000"
+
+
+class TestGetViewBox:
+
+    def test_has_view_box(self) -> None:
+        assert get_view_box(get_svg("view_box.svg")) == ViewBox(1, 2, 3, 4)
+    
+    def test_no_view_box(self) -> None:
+        assert get_view_box(get_svg("no_view_box.svg")) is None
+
+
+class TestGetInkscapePages:
+    
+    def test_pages(self) -> None:
+        assert get_inkscape_pages(get_svg("multiple_pages.svg")) == [
+            ViewBox(10, 20, 30, 40),
+            ViewBox(50, 20, 50, 60),
+        ]
+    
+    def test_old_inkscape_file(self) -> None:
+        assert get_inkscape_pages(get_svg("old_inkscape_file.svg")) == []
+
