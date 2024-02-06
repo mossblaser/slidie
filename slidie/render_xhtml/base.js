6cd70720b1b1e49cceb90bb77415ab0b9fe06332
Jonathan Heathcote <mail@jhnet.co.uk>
2024-02-06 22:07:38 +0000

    Make thumbnails and notes resizable

--- slidie/render_xhtml/base.js
+++ slidie/render_xhtml/base.js
@@ -1065,6 +1065,87 @@ function showPresenterView(stepper, stopwatch, clickForwardingTarget) {
   return wnd;
 }
 
+/**
+ * Make a HTML element resizable by dragging any visible CSS border.
+ *
+ * NB: Currently only top/right border dragging is supported.
+ */
+function resizeOnBorderDrag(elem) {
+  // Show resize cursor when mouse over borders
+  elem.addEventListener("mousemove", evt => {
+    if (evt.offsetX - elem.clientWidth >= 0) {
+      elem.style.cursor = "ew-resize";
+    } else if (evt.offsetY < 0) {
+      elem.style.cursor = "ns-resize";
+    } else {
+      // NB: Bottom/left edges not implemented...
+      elem.style.cursor = "auto";
+    }
+  });
+  
+  // Implement resizing logic
+  elem.addEventListener("mousedown", evt => {
+    const style = getComputedStyle(elem);
+    
+    // Detect which adjustment is being made
+    let adjust;
+    let scale;
+    if (style.borderRightStyle == "solid") {
+      if (evt.offsetX - elem.clientWidth < 0) {
+        return; // Click not on right border
+      }
+      adjust = "width";
+      scale = 1;
+    } else if (style.borderTopStyle == "solid") {
+      if (evt.offsetY >= 0) {
+        return; // Click not on top border
+      }
+      adjust = "height";
+      scale = -1;
+    } else {
+      console.warn("Not implemented: Adjusting bottom/left edges!");
+    }
+    
+    evt.preventDefault();
+    evt.stopPropagation();
+    
+    let lastX = evt.clientX;
+    let lastY = evt.clientY;
+    let width = elem.offsetWidth
+    let height = elem.offsetHeight;
+    
+    // Adjust height/width accordingly
+    function onMouseMove(evt) {
+      const deltaX = evt.clientX - lastX;
+      const deltaY = evt.clientY - lastY;
+      lastX = evt.clientX;
+      lastY = evt.clientY;
+      
+      if (adjust == "width") {
+        width += deltaX * scale;
+        elem.style.width = `${width}px`;
+      } else if (adjust == "height") {
+        height += deltaY * scale;
+        elem.style.height = `${height}px`;
+      }
+      
+      evt.preventDefault();
+      evt.stopPropagation();
+    }
+    function onMouseUp(evt) {
+      window.removeEventListener("mousemove", onMouseMove);
+      window.removeEventListener("mouseup", onMouseMove);
+      window.removeEventListener("mouseup", onMouseUp);
+      evt.preventDefault();
+      evt.stopPropagation();
+    }
+    
+    window.addEventListener("mousemove", onMouseMove);
+    window.addEventListener("mouseup", onMouseMove);  // Apply final position
+    window.addEventListener("mouseup", onMouseUp);
+  });
+}
+
 /******************************************************************************/
 
 function setup() {
@@ -1084,6 +1165,10 @@ function setup() {
     slide.style.width = "100%";
     slide.style.height = "100%";
   }
+  
+  // Allow user resizing of thumbnails and notes
+  resizeOnBorderDrag(document.getElementById("thumbnails"));
+  resizeOnBorderDrag(document.getElementById("notes"));
   
   setupMagicVideoPlayback(slides);
   
