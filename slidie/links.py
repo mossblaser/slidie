fbf741ee0885fe95d38e5cab7c96bcedccc5e337
Jonathan Heathcote <mail@jhnet.co.uk>
2024-02-26 15:08:41 +0000

    Add resolve_link Python implementation
    This implementation (and tests) are essentially cribbed off the Javascript
    version in the XHTML renderer.

--- slidie/links.py
+++ slidie/links.py
@@ -103,3 +103,99 @@ def annotate_slide_id_from_magic(magic: dict[str, list[MagicText]]) -> None:
     # Annotate SVG
     svg = id_magic[0].parents[0]
     svg.attrib[f"{{{SLIDIE_NAMESPACE}}}id"] = slide_id
+
+
+# fmt: off
+LINK_REGEX = re.compile(
+    "^#"
+    # Slide spec
+    "(?:"
+        "(?P<slide_index>[0-9]+)"
+        "|"
+        "(?P<slide_id>[^0-9#@<][^#@<]*)"
+    ")?"
+    # Build step spec
+    "(?:"
+        "(?:#(?P<step_index>[0-9]+))"
+        "|"
+        "(?:<(?P<step_number>[-+]?[0-9]+)>)"
+        "|"
+        "(?:@(?P<step_tag>[^\\s<>.@]+))"
+    ")?"
+    "$"
+)
+# fmt: on
+"""
+Regular expression matching valid link syntax.
+"""
+
+
+def resolve_link(
+    link: str,
+    slide_ids: dict[str, int],
+    slide_step_numbers: list[list[int]],
+    slide_build_tags: list[dict[str, set[int]]],
+    current_slide_index: int,
+) -> tuple[int, int] | None:
+    """
+    If passed a valid link, returns the (slide_index, step_index) for the
+    referenced slide.
+
+    Parameters
+    ==========
+    link: str
+        The link to resolve (including leading hash).
+    slide_ids: {"slide_id": slide_index, ...}
+        Lookup for all defined slide IDs.
+    slide_step_numbers: [[step_number, ...], ...]
+        For each slide, an enumeration of the user-defined step numbers.
+    slide_build_tags: [{"tag": {step_number}}, ...]
+        For each slide, a lookup from tag to step numbers (not indices) with
+        that tag.
+    current_slide_index: int
+        The index of the currently displayed slide (not step).
+
+    Returns
+    =======
+    (slide_index, step_index)
+        If the provided link is valid.
+    None
+        Otherwise.
+    """
+    match = LINK_REGEX.fullmatch(link)
+    if not match:
+        return None
+
+    # Work out the slide index
+    if slide_index_str := match.group("slide_index"):
+        slide_index = int(slide_index_str) - 1
+    elif slide_id := match.group("slide_id"):
+        if slide_id in slide_ids:
+            slide_index = slide_ids[slide_id]
+        else:
+            return None  # Unknown slide ID
+    else:
+        slide_index = current_slide_index  # Default to current slide
+
+    # Work out step index
+    if step_index_str := match.group("step_index"):
+        step_index = int(step_index_str) - 1
+    elif step_number_str := match.group("step_number"):
+        try:
+            step_index = slide_step_numbers[slide_index].index(int(step_number_str))
+        except IndexError:
+            step_index = 0  # For out-of-range slide indices always resolve to zero
+        except ValueError:
+            step_index = 0  # Treat out-of-range step number as zero
+    elif step_tag := match.group("step_tag"):
+        try:
+            step_number = min(slide_build_tags[slide_index].get(step_tag, set()))
+            step_index = slide_step_numbers[slide_index].index(step_number)
+        except IndexError:
+            step_index = 0  # For out-of-range slide indices always resolve to zero
+        except ValueError:
+            step_index = 0  # Treat tags with no steps as zero
+    else:
+        step_index = 0  # Default to first step
+
+    return (slide_index, step_index)
--- tests/test_links.py
+++ tests/test_links.py
@@ -8,6 +8,7 @@ from slidie.links import (
     MultipleIdsError,
     InvalidIdError,
     annotate_slide_id_from_magic,
+    resolve_link,
 )
 
 
@@ -83,3 +84,78 @@ class TestAnnotateSlideIdFromMagic:
         }
         annotate_slide_id_from_magic(magic)
         assert svg.attrib[f"{{{SLIDIE_NAMESPACE}}}id"] == slide_id
+
+
+class TestResolveLink:
+    @pytest.mark.parametrize(
+        "link",
+        [
+            # No hash prefix
+            "",
+            "123",
+            # Multiple step specs
+            "##1#2",
+            "##1<2>",
+            "##1@foo",
+            "#<1>#2",
+            "#<1><2>",
+            "#<1>@two",
+            "#@one@two",
+            # Unknown ID
+            "#who-knows",
+        ],
+    )
+    def test_invalid(self, link: str) -> None:
+        assert resolve_link(link, {}, [[0]], [{}], 0) is None
+
+    @pytest.mark.parametrize(
+        "link, exp_slide_index, exp_step_index",
+        [
+            # Current slide
+            ("#", 1, 0),
+            # Current slide, step index
+            ("##2", 1, 1),
+            # Current slide, step number
+            ("#<-1>", 1, 1),
+            # Current slide, step tag
+            ("#@first-step", 1, 0),
+            # Numbered slide
+            ("#1", 0, 0),
+            # Numbered slide with step
+            ("#1#2", 0, 1),
+            # Slide by ID
+            ("#third-slide", 2, 0),
+            # Slide by ID with step
+            ("#third-slide#2", 2, 1),
+            # Tag with multiple steps
+            ("#1@last-two-steps", 0, 1),
+            # Unknown tag
+            ("#3@nope", 2, 0),
+            # Tag on out of range slide
+            ("#99@nope", 98, 0),
+            # Unknown step number
+            ("#3<99>", 2, 0),
+            # Step number on out of range slide
+            ("#99<99>", 98, 0),
+        ],
+    )
+    def test_valid(self, link: str, exp_slide_index: int, exp_step_index: int) -> None:
+        assert resolve_link(
+            link,
+            slide_ids={"third-slide": 2},
+            slide_step_numbers=[
+                [-1, 0, 1],
+                [-2, -1, 0],
+                [0, 1],
+            ],
+            slide_build_tags=[
+                {
+                    "first-step": {-1},
+                    "last-step": {0},
+                    "last-two-steps": {0, 1},
+                },
+                {"first-step": {-2}},
+                {},
+            ],
+            current_slide_index=1,
+        ) == (exp_slide_index, exp_step_index)
