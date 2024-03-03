bb77a383f9328d46f47483df7e7d81eae941a5f9
Jonathan Heathcote <mail@jhnet.co.uk>
2024-03-03 21:18:58 +0000

    Refactor setupMagicVideoPlayback to operate on one slide

--- slidie/render_xhtml/base.js
+++ slidie/render_xhtml/base.js
@@ -771,29 +771,27 @@ function loadNotes(stepper, container) {
  * Setup automatic video play/pause on slide entry/exit for videos inserted
  * using magic text.
  */
-function setupMagicVideoPlayback(slides) {
-  for (const slide of slides) {
-    for (const video of slide.getElementsByTagNameNS(ns("xhtml"), "video")) {
-      if (video.hasAttributeNS(ns("slidie"), "magic")) {
-        const start = parseFloat(video.getAttributeNS(ns("slidie"), "start") || "0");
-        const steps = JSON.parse(video.getAttributeNS(ns("slidie"), "steps") || "null");
-        
-        video.currentTime = start;
-        
-        slide.addEventListener("stepchange", ({stepNumber}) => {
-          if (steps === null || steps.indexOf(stepNumber) >= 0) {
-            video.play();  // NB: NOP if already playing
-          } else {
-            video.pause();
-            video.currentTime = start;
-          }
-        });
-        
-        slide.addEventListener("slideexit", () => {
+function setupMagicVideoPlayback(slide) {
+  for (const video of slide.getElementsByTagNameNS(ns("xhtml"), "video")) {
+    if (video.hasAttributeNS(ns("slidie"), "magic")) {
+      const start = parseFloat(video.getAttributeNS(ns("slidie"), "start") || "0");
+      const steps = JSON.parse(video.getAttributeNS(ns("slidie"), "steps") || "null");
+      
+      video.currentTime = start;
+      
+      slide.addEventListener("stepchange", ({stepNumber}) => {
+        if (steps === null || steps.indexOf(stepNumber) >= 0) {
+          video.play();  // NB: NOP if already playing
+        } else {
           video.pause();
           video.currentTime = start;
-        });
-      }
+        }
+      });
+      
+      slide.addEventListener("slideexit", () => {
+        video.pause();
+        video.currentTime = start;
+      });
     }
   }
 }
@@ -1288,7 +1286,7 @@ function setup() {
   resizeOnBorderDrag(document.getElementById("thumbnails"));
   resizeOnBorderDrag(document.getElementById("notes"));
   
-  setupMagicVideoPlayback(slides);
+  slides.map(setupMagicVideoPlayback);
   
   if (slides[0].hasAttributeNS(ns("slidie"), "title")) {
     const title = slides[0].getAttributeNS(ns("slidie"), "title");
