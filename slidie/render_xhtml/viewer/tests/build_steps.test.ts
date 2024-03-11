88316ab67544348740fda5037f34f606f17ff2dc
Jonathan Heathcote <mail@jhnet.co.uk>
2024-03-11 21:08:06 +0000

    Port build step locating functions to TS
    Also added new tests.
    
    findBuildSteps now returns both step numbers and step indices (it used to only
    return numbers under the name 'steps' -- revised for consistency and utility).
    
    Function names could probably do with revising at some point in the future.

---
+++ slidie/render_xhtml/viewer/tests/build_steps.test.ts
@@ -0,0 +1,123 @@
+import { test } from "node:test";
+import { strict as assert } from "node:assert";
+
+import ns from "../ts/xml_namespaces.ts";
+import { getProcessedSvg } from "./svgs.ts";
+
+import {
+  findBuildSteps,
+  layerStepIndices,
+  layerStepTags,
+  findElementBuildSteps,
+} from "../ts/build_steps.ts";
+
+test("build_steps", async (t) => {
+  const empty = await getProcessedSvg("empty.svg");
+  const negativeBuildStepNumber = await getProcessedSvg(
+    "negative_build_step_number.svg",
+  );
+
+  await t.test("findBuildSteps", async (t) => {
+    await t.test("no steps", async (t) => {
+      assert.deepEqual(findBuildSteps(empty), []);
+    });
+
+    await t.test("tags and negative steps", async (t) => {
+      const buildSteps = findBuildSteps(negativeBuildStepNumber);
+      assert.deepEqual(
+        buildSteps.map(({ elem, ...rest }) => ({
+          layer: elem.getAttributeNS(ns("inkscape"), "label"),
+          ...rest,
+        })),
+        [
+          {
+            layer: "Third <+-> @bar",
+            stepNumbers: [1],
+            steps: [2],
+            tags: ["bar"],
+          },
+          {
+            layer: "Second <.> @foo @bar",
+            stepNumbers: [0],
+            steps: [1],
+            tags: ["bar", "foo"],
+          },
+          {
+            layer: "Always (explicit) <@foo.before->",
+            stepNumbers: [-1, 0, 1],
+            steps: [0, 1, 2],
+            tags: [],
+          },
+        ],
+      );
+    });
+  });
+
+  await t.test("layerStepIndices", async (t) => {
+    await t.test("no steps", async (t) => {
+      assert.deepEqual(layerStepIndices(findBuildSteps(empty)), [0]);
+    });
+    await t.test("negative steps", async (t) => {
+      assert.deepEqual(
+        layerStepIndices(findBuildSteps(negativeBuildStepNumber)),
+        [0, 1, 2],
+      );
+    });
+  });
+
+  await t.test("layerStepTags", async (t) => {
+    await t.test("no tags", async (t) => {
+      assert.deepEqual(layerStepTags(findBuildSteps(empty)), new Map());
+    });
+    await t.test("has tags", async (t) => {
+      assert.deepEqual(
+        layerStepTags(findBuildSteps(negativeBuildStepNumber)),
+        new Map([
+          ["foo", [1]],
+          ["bar", [1, 2]],
+        ]),
+      );
+    });
+  });
+
+  await t.test("findElementBuildSteps", async (t) => {
+    await t.test("not on build step", async (t) => {
+      assert.deepEqual(
+        findElementBuildSteps(
+          negativeBuildStepNumber.ownerDocument.getElementById(
+            "not-on-build-step",
+          ) as Element,
+        ),
+        null,
+      );
+    });
+
+    await t.test("immediate child of layer", async (t) => {
+      assert.deepEqual(
+        findElementBuildSteps(
+          negativeBuildStepNumber.ownerDocument.getElementById(
+            "on-second",
+          ) as Element,
+        ),
+        {
+          stepNumbers: [0],
+          tags: ["bar", "foo"],
+        },
+      );
+    });
+
+    await t.test("nested child of layer", async (t) => {
+      assert.deepEqual(
+        findElementBuildSteps(
+          negativeBuildStepNumber.ownerDocument.getElementById(
+            "in-group-on-second",
+          ) as Element,
+        ),
+        {
+          stepNumbers: [0],
+          tags: ["bar", "foo"],
+        },
+      );
+    });
+  });
+});
---
+++ slidie/render_xhtml/viewer/ts/build_steps.ts
@@ -0,0 +1,133 @@
+/**
+ * The following functions are used for extracting information slidie specific
+ * information from SVGs (e.g. build steps, thumbnails etc.).
+ */
+
+import ns from "./xml_namespaces.ts";
+
+interface BuildStepVisibility {
+  // The SVG element whose visibility is controlled by the build step
+  elem: SVGElement;
+
+  // The step numbers (not necessarily zero-indexed) at which this build step
+  // is visible.
+  stepNumbers: number[];
+
+  // The step indices (zero-indexed) at which the build step is visible.
+  steps: number[];
+
+  // The build step tags assigned to this element (if any)
+  tags: string[];
+}
+
+/**
+ * Given an SVG root node, find all SVG elements containing a slidie
+ * 'steps' attribute.
+ */
+export function findBuildSteps(svgRoot: SVGSVGElement): BuildStepVisibility[] {
+  // Find all steps
+  const out: BuildStepVisibility[] = [];
+  for (const elem of svgRoot.querySelectorAll("*")) {
+    if (
+      elem.namespaceURI == ns("svg") &&
+      elem.hasAttributeNS(ns("slidie"), "steps")
+    ) {
+      const stepNumbers = JSON.parse(
+        elem.getAttributeNS(ns("slidie"), "steps")!,
+      ) as number[];
+      const tags = JSON.parse(
+        elem.getAttributeNS(ns("slidie"), "tags") || "[]",
+      ) as string[];
+      out.push({
+        elem: elem as SVGElement,
+        stepNumbers,
+        tags,
+        steps: [], // Placeholder, populated below
+      });
+    }
+  }
+
+  // Add zero-indexed step indices
+  const firstStepNumber = Math.min(
+    ...out
+      .map(({ stepNumbers }) => stepNumbers)
+      .flat()
+      .concat([0]),
+  );
+  for (const obj of out) {
+    obj.steps = obj.stepNumbers.map((n) => n - firstStepNumber);
+  }
+
+  return out;
+}
+
+/**
+ * Given an array [{steps, ...}, ...] (e.g. from findBuildSteps), returns an
+ * array enumerating the full range of step indices used by any build specs
+ * provided.
+ */
+export function layerStepIndices(layerSteps: { steps: number[] }[]): number[] {
+  const allSteps = layerSteps
+    .map(({ steps }) => steps)
+    .flat()
+    .concat([0]);
+  const last = Math.max(...allSteps);
+
+  const out = [];
+  for (let i = 0; i <= last; i++) {
+    out.push(i);
+  }
+  return out;
+}
+
+/**
+ * Given an array [{steps, tags}, ...], returns an Map from tag names to
+ * (zero-indexed) step indices.
+ */
+export function layerStepTags(
+  layerSteps: { steps: number[]; tags: string[] }[],
+): Map<string, number[]> {
+  const map = new Map();
+  for (const { steps, tags } of layerSteps) {
+    for (const tag of tags) {
+      if (!map.has(tag)) {
+        map.set(tag, []);
+      }
+      for (const step of steps) {
+        if (map.get(tag).indexOf(step) == -1) {
+          map.get(tag).push(step);
+        }
+      }
+      map.get(tag).sort();
+    }
+  }
+
+  return map;
+}
+
+/**
+ * Given an SVG element, returns a {stepNumbers, tags} for the build steps of
+ * the nearest parent element with build steps specified. If no parent has any
+ * build steps specified, returns null.
+ */
+export function findElementBuildSteps(
+  elem: Element,
+): { stepNumbers: number[]; tags: string[] } | null {
+  let curElem: Element | null = elem;
+  while (curElem) {
+    if (
+      curElem.namespaceURI == ns("svg") &&
+      curElem.hasAttributeNS(ns("slidie"), "steps")
+    ) {
+      const stepNumbers = JSON.parse(
+        curElem.getAttributeNS(ns("slidie"), "steps")!,
+      );
+      const tags = JSON.parse(
+        curElem.getAttributeNS(ns("slidie"), "tags") || "[]",
+      );
+      return { stepNumbers, tags };
+    }
+    curElem = curElem.parentElement;
+  }
+  return null;
+}
