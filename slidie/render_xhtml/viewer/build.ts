72c495632cfce268b046b1e92b49e2097a6bea2f
Jonathan Heathcote <mail@jhnet.co.uk>
2024-03-19 14:36:36 +0000

    Type-check everything before all builds.

--- slidie/render_xhtml/viewer/build.ts
+++ slidie/render_xhtml/viewer/build.ts
@@ -23,14 +23,30 @@ const buildOptions = {
   entryPoints: ["./ts/index.js"],
   outdir: ".",
   bundle: true,
-  sourcemap: "inline" as "inline",
+  // Ideally we would use 'inline' source map however both Firefox and Chrome's
+  // handling of inline sourcemaps is extremely buggy to the point of not being
+  // usable for debugging at present. As a result we use a linked sourcemap
+  // here (which will be loaded in debug builds of the viewer where the JS is
+  // not inlined). In non-debug builds, the magic 'sourceMappingURL' comment
+  // will be amended to include the data-URL version of the source map
+  // instead. For present day browsers this at least makes the source available
+  // in the debug panel and maybe for future browsers will actually be useful
+  // for debugging too!
+  sourcemap: "linked" as "linked",
   banner: {
     js:
-      "// The following Javascript is built from the Typescript sources in\n" +
-      "//   slidie/render_xhtml/viewer/ts\n" +
-      "// Unfortunately comments and type information are lost in the process.\n" +
-      "// Sorry about that. There is an inline source map, though, and the\n" +
-      "// original sources should be online at https://github.com/mossblaser/slidie.\n",
+      "\n" +
+      "// Though the code below is the mangled result of the Typescript\n" +
+      "// compiler and esbuild bundler (hence the absent comments) it isn't\n" +
+      "// minified so may still be readable... The full Typescript sources are\n" +
+      "// embedded in the source map at the end of the script file and should\n" +
+      "// show up in your Browser's debug tools. At the time of writing (2024)\n" +
+      "// most browsers, however, still fail to resolve names/line numbers\n" +
+      "// using the inline source maps within an inline script tag. Sorry.\n" +
+      "//\n" +
+      "// The following Javascript is built from the Typescript sources in the\n" +
+      "// `slidie/render_xhtml/viewer/ts` directory of the Slidie source code\n" +
+      "// which should be online at https://github.com/mossblaser/slidie.\n",
   },
 };
 
