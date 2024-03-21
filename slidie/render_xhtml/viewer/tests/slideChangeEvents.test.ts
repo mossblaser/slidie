4ec76a7397add5832de527cc94b1ded1f74848d1
Jonathan Heathcote <mail@jhnet.co.uk>
2024-03-21 07:58:17 +0000

    Port generation slide change events
    Now encapsulated separately from the stepper.

---
+++ slidie/render_xhtml/viewer/tests/slideChangeEvents.test.ts
@@ -0,0 +1,173 @@
+import { test, mock } from "node:test";
+import { strict as assert } from "node:assert";
+
+import { Stepper } from "../ts/stepper.ts";
+import {
+  SlideChangeEvent,
+  connectStepperToSlideEvents,
+} from "../ts/slideChangeEvents.ts";
+
+test("slideChangeEvents", async (t) => {
+  // A dummy setup roughly mimicing a show consisting of empty.svg followed by
+  // negative_build_step_number.svg.
+  function makeSlides() {
+    return {
+      svgs: [
+        { dispatchEvent: mock.fn((evt: SlideChangeEvent) => {}) },
+        { dispatchEvent: mock.fn((evt: SlideChangeEvent) => {}) },
+      ] as any[] as SVGSVGElement[],
+      buildStepCounts: [1, 3],
+      buildStepNumbers: [[0], [-1, 0, 1]],
+      buildStepTags: [
+        new Map(),
+        new Map([
+          ["foo", [1]],
+          ["bar", [1, 2]],
+        ]),
+      ],
+    };
+  }
+
+  /**
+   * Return newly received SlideChangeEvents dispatched to the given slide
+   * since the last call to this function.
+   */
+  function dispatches(
+    slides: { svgs: SVGSVGElement[] },
+    slide: number,
+  ): SlideChangeEvent[] {
+    // Excuse the slack typing... the mock types are not exported by the node
+    // and I'm too lazy to do anything more precise here...
+    const mock = (slides.svgs[slide] as any).dispatchEvent.mock;
+    const events = mock.calls.map(
+      (call: any) => call.arguments[0],
+    ) as SlideChangeEvent[];
+    mock.resetCalls();
+    return events;
+  }
+
+  await t.test("connectStepperToSlideEvents", async (t) => {
+    await t.test("event sequencing", async (t) => {
+      const slides = makeSlides();
+      const stepper = new Stepper(slides.buildStepCounts);
+      connectStepperToSlideEvents(stepper, slides);
+
+      // Should initially enter the selected slide
+      assert.deepEqual(
+        dispatches(slides, 0).map((evt) => evt.type),
+        ["slideenter"],
+      );
+      assert.deepEqual(
+        dispatches(slides, 1).map((evt) => evt.type),
+        [],
+      );
+
+      // Changing slide
+      stepper.show(1, 0);
+      assert.deepEqual(
+        dispatches(slides, 0).map((evt) => evt.type),
+        ["slideleave"],
+      );
+      assert.deepEqual(
+        dispatches(slides, 1).map((evt) => evt.type),
+        ["slideenter"],
+      );
+
+      // Changing step within a slide
+      stepper.show(1, 1);
+      assert.deepEqual(
+        dispatches(slides, 0).map((evt) => evt.type),
+        [],
+      );
+      assert.deepEqual(
+        dispatches(slides, 1).map((evt) => evt.type),
+        ["stepchange"],
+      );
+
+      // Blanking
+      stepper.toggleBlank();
+      assert.deepEqual(
+        dispatches(slides, 0).map((evt) => evt.type),
+        [],
+      );
+      assert.deepEqual(
+        dispatches(slides, 1).map((evt) => evt.type),
+        ["slideblank"],
+      );
+
+      // Unblanking
+      stepper.toggleBlank();
+      assert.deepEqual(
+        dispatches(slides, 0).map((evt) => evt.type),
+        [],
+      );
+      assert.deepEqual(
+        dispatches(slides, 1).map((evt) => evt.type),
+        ["slideunblank"],
+      );
+
+      // Implicit unblanking
+      stepper.toggleBlank();
+      assert.deepEqual(
+        dispatches(slides, 0).map((evt) => evt.type),
+        [],
+      );
+      assert.deepEqual(
+        dispatches(slides, 1).map((evt) => evt.type),
+        ["slideblank"],
+      );
+      stepper.show(1, 0); // During step change
+      assert.deepEqual(
+        dispatches(slides, 0).map((evt) => evt.type),
+        [],
+      );
+      assert.deepEqual(
+        dispatches(slides, 1).map((evt) => evt.type),
+        ["stepchange", "slideunblank"],
+      );
+      stepper.toggleBlank();
+      assert.deepEqual(
+        dispatches(slides, 0).map((evt) => evt.type),
+        [],
+      );
+      assert.deepEqual(
+        dispatches(slides, 1).map((evt) => evt.type),
+        ["slideblank"],
+      );
+      stepper.show(0, 0); // During slide change
+      assert.deepEqual(
+        dispatches(slides, 0).map((evt) => evt.type),
+        ["slideenter"],
+      );
+      assert.deepEqual(
+        dispatches(slides, 1).map((evt) => evt.type),
+        ["slideleave", "slideunblank"],
+      );
+    });
+  });
+
+  await t.test("SlideChangeEvent properties", async (t) => {
+    const slides = makeSlides();
+    const stepper = new Stepper(slides.buildStepCounts);
+    connectStepperToSlideEvents(stepper, slides);
+
+    // No tags for this step
+    const events0 = dispatches(slides, 0);
+    assert.deepEqual(events0.length, 1);
+    assert.deepEqual(events0[0].type, "slideenter");
+    assert.deepEqual(events0[0].slide, 0);
+    assert.deepEqual(events0[0].step, 0);
+    assert.deepEqual(events0[0].stepNumber, 0);
+    assert.deepEqual(events0[0].tags, []);
+
+    // No step with tags and different start offset for step numbers
+    stepper.show(1, 2);
+    const events1 = dispatches(slides, 1);
+    assert.deepEqual(events1.length, 1);
+    assert.deepEqual(events1[0].type, "slideenter");
+    assert.deepEqual(events1[0].slide, 1);
+    assert.deepEqual(events1[0].step, 2);
+    assert.deepEqual(events1[0].stepNumber, 1);
+    assert.deepEqual(events1[0].tags, ["bar"]);
+  });
+});
---
+++ slidie/render_xhtml/viewer/ts/slideChangeEvents.ts
@@ -0,0 +1,134 @@
+/**
+ * This module implements the events which are dispatched to slides to inform
+ * them of progress during a presentation.
+ *
+ * The following events (all dispatching a SlideChangeEvent object) are
+ * defined which are sent to the <svg> element of the effected slide:
+ *
+ * * slideenter: When entering a slide (but not when the step changes)
+ * * slideleave: When leaving a slide. NB: Contains the slide details of the
+ *   slide which replaces it.
+ * * stepchange: When the step changes within the slide (but not when initially
+ *   entering the slide.
+ * * slideblank: When the screen is blanked
+ * * slideunblank: When the screen is unblanked. If the slide is unblanked
+ *   implicitly (i.e. when moving to another next slide), the event will
+ *   contain the details of the slide which replaces this slide.
+ *
+ * Where multiple events are arise from a single change, the events are
+ * dispatched in the order listed above.
+ */
+
+import { Stepper, StepperState } from "./stepper.ts";
+
+/**
+ * Valid event type names for a slide change event.
+ */
+export type SlideChangeEventType =
+  | "slideenter"
+  | "slideleave"
+  | "stepchange"
+  | "slideblank"
+  | "slideunblank";
+
+/**
+ * An event fired when the slide (or step) changes. Contains various fields
+ * which may be useful to embedded scripts.
+ */
+export class SlideChangeEvent extends Event {
+  constructor(
+    type: SlideChangeEventType,
+    state: StepperState,
+    slides: {
+      buildStepNumbers: number[][];
+      buildStepTags: Map<string, number[]>[];
+    },
+  ) {
+    super(type, {
+      // Don't bubble out of the SVG's shadow DOM
+      composed: false,
+      // The change has already occurred by the time this event is fired
+      cancelable: false,
+      // Allow bubbling up to the window of the shadow DOM
+      bubbles: true,
+    });
+
+    this.slide = state.slide;
+    this.step = state.step;
+
+    this.stepNumber = slides.buildStepNumbers[state.slide][state.step];
+
+    this.tags = [];
+    for (const [tag, steps] of slides.buildStepTags[state.slide].entries()) {
+      if (steps.indexOf(state.step) >= 0) {
+        this.tags.push(tag);
+      }
+    }
+  }
+
+  // The (zero-based) slide index
+  slide: number;
+
+  // The (zero-based) step index
+  step: number;
+
+  // The (not-necessarily zero-based) step number
+  stepNumber: number;
+
+  // The tags associated with any currently visible build steps.
+  tags: string[];
+}
+
+/**
+ * Dispatch slide change events in response to changes made by the provided
+ * stepper.
+ */
+export function connectStepperToSlideEvents(
+  stepper: Stepper,
+  slides: {
+    svgs: SVGSVGElement[];
+    buildStepNumbers: number[][];
+    buildStepTags: Map<string, number[]>[];
+  },
+) {
+  function dispatchEvents(state: StepperState, lastState: StepperState | null) {
+    const svg = slides.svgs[state.slide];
+
+    // Enter and leave
+    if (lastState === null || state.slide != lastState.slide) {
+      svg.dispatchEvent(new SlideChangeEvent("slideenter", state, slides));
+
+      if (lastState !== null) {
+        const lastSvg = slides.svgs[lastState.slide];
+        lastSvg.dispatchEvent(
+          new SlideChangeEvent("slideleave", state, slides),
+        );
+      }
+    }
+
+    // Step change
+    if (
+      lastState !== null &&
+      state.slide == lastState.slide &&
+      state.step != lastState.step
+    ) {
+      svg.dispatchEvent(new SlideChangeEvent("stepchange", state, slides));
+    }
+
+    // Blanking
+    if (lastState !== null) {
+      if (state.blanked && !lastState.blanked) {
+        svg.dispatchEvent(new SlideChangeEvent("slideblank", state, slides));
+      }
+      if (!state.blanked && lastState.blanked) {
+        const lastSvg = slides.svgs[lastState.slide];
+        lastSvg.dispatchEvent(
+          new SlideChangeEvent("slideunblank", state, slides),
+        );
+      }
+    }
+  }
+
+  dispatchEvents(stepper.state, null);
+  stepper.onChange(dispatchEvents);
+}
