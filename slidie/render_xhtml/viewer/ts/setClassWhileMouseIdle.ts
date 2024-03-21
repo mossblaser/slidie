d1a8900b8c87f4c658d8447a7d32f8e185f1e982
Jonathan Heathcote <mail@jhnet.co.uk>
2024-03-21 21:38:15 +0000

    Port setClassWhileMouseIdle

---
+++ slidie/render_xhtml/viewer/ts/setClassWhileMouseIdle.ts
@@ -0,0 +1,22 @@
+/**
+ * Sets the specified class on the specified element whenever the mouse hasn't
+ * moved over it in the last timeout milliseconds.
+ */
+export function setClassWhileMouseIdle(
+  elem: HTMLElement,
+  className: string = "mouse-idle",
+  timeout: number = 2000,
+) {
+  let timeoutId: ReturnType<typeof setTimeout> | null = null;
+  elem.addEventListener("mousemove", (evt: MouseEvent) => {
+    elem.classList.remove(className);
+
+    if (timeoutId !== null) {
+      clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+      elem.classList.add(className);
+      timeoutId = null;
+    }, timeout);
+  });
+}
