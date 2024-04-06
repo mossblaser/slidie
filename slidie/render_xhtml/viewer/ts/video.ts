26a545b74e1463ee1f7d6307c83e4551555d1a59
Jonathan Heathcote <mail@jhnet.co.uk>
2024-04-06 20:38:00 +0100

    Port video playback control to TS

---
+++ slidie/render_xhtml/viewer/ts/video.ts
@@ -0,0 +1,44 @@
+import ns from "./xmlNamespaces.ts";
+
+import { SlideChangeEvent } from "./slideChangeEvents.ts";
+
+/**
+ * Setup automatic video play/pause on slide entry/exit for videos inserted
+ * using magic text.
+ */
+export function setupMagicVideoPlayback(slide: SVGSVGElement) {
+  for (const video of slide.getElementsByTagNameNS(
+    ns("xhtml"),
+    "video",
+  ) as HTMLCollectionOf<HTMLVideoElement>) {
+    if (video.hasAttributeNS(ns("slidie"), "magic")) {
+      const start = parseFloat(
+        video.getAttributeNS(ns("slidie"), "start") || "0",
+      );
+      const stepNumbers = JSON.parse(
+        video.getAttributeNS(ns("slidie"), "steps") || "null",
+      );
+
+      video.currentTime = start;
+
+      const onEnterOrChange = ({ stepNumber }: SlideChangeEvent) => {
+        if (stepNumbers === null || stepNumbers.indexOf(stepNumber) >= 0) {
+          video.play(); // NB: NOP if already playing
+        } else {
+          video.pause();
+          video.currentTime = start;
+        }
+      };
+
+      // @ts-expect-error: I CBA setting up type checking for custom events for now
+      slide.addEventListener("slideenter", onEnterOrChange);
+      // @ts-expect-error: I CBA setting up type checking for custom events for now
+      slide.addEventListener("stepchange", onEnterOrChange);
+
+      slide.addEventListener("slideleave", () => {
+        video.pause();
+        video.currentTime = start;
+      });
+    }
+  }
+}
